e927c4d50be32764d692f19c60f808c4da1ce67b
RapliVX <raffikun1203@gmail.com>
2026-02-18 02:02:43 +0700

    Revert "BACKPORT: f2fs: allow to change discard policy based on cached discard cmds"
    This reverts commit 835ac6345fedbf733cdb6f4ed9202cc85ede8acb.

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1703,10 +1703,8 @@ static int issue_discard_thread(void *data)
 			wait_ms = dpolicy.max_interval;
 			continue;
 		}
-
-		if (sbi->gc_mode == GC_URGENT ||
-			!f2fs_available_free_memory(sbi, DISCARD_CACHE))
-			__init_discard_policy(sbi, &dpolicy, DPOLICY_FORCE, 1);
+		if (!atomic_read(&dcc->discard_cmd_cnt))
+			continue;
 
 		sb_start_intwrite(sbi->sb);
 
